0034d4906c231d9064d2f2c16c4bd0e74b423315
Aleksandr <i@afeklin.ru>
2022-09-16 15:14:27 +0300

    Добавлена папка с программой

---
+++ Program/Program.cs
@@ -0,0 +1,53 @@
+﻿string[] array1 = new string[6] { "222", "`223", "-2222", "-d899", "ads", "33" };
+string[] array2 = new string[array1.Length];
+void NewArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void ViewArr(string[] array)
+{
+    string result = "[";
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            result += array[i] + ", ";
+        }
+        if (i == array.Length - 1)
+        {
+            result += array[i] + "]";
+        }
+    }
+    Console.Write("Исходный массив : ");
+    Console.WriteLine(result);
+}
+void PrintArray(string[] array)
+{
+    string result = "[";
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length -3)
+        {
+            result += array[i] + ", ";
+        }
+        if (i == array.Length -3)
+        {
+            result += array[i] + "]";
+        }
+    }
+    Console.Write("Готовый массив : ");
+    Console.WriteLine(result);
+}
+ViewArr(array1);
+Console.WriteLine();
+NewArray(array1, array2);
+PrintArray(array2);
+
